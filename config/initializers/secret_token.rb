d779b6d56d44573f05ed187bb4a00b27e8ffcb8f
Doug Zerafa <doug.zerafa@gmail.com>
2013-03-02 09:34:52 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DougApp::Application.config.secret_token = 'c525e9ceed2cbd4a886252a3fdac210f0690748ecd02dddc140b389835fb33426f21c571387bfd5768ddee8536ef7a9108202d62fc9455887b887d4ee5af66b9'
